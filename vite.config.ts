a0fa889d0e0872296a6a6072cd84365fc17b33d8
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-09-28 08:50:54 -0600

    refactor frontend code into its own folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'node:path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/frontend')
+    }
+  },
   build: {
     target: 'esnext',
   },
